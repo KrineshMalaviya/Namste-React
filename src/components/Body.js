84b88bd58c5e95080b7fd50f1ae760e03f1c2e6d
Krinesh Malaviya <krinesh.malaviya@gmail.com>
2025-12-16 21:24:37 +0530

    Swiggy Api issue solve

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,39 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import resList from "../utills/mockdata";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurant, setlistOfRestaurant] = useState(resList);
-  const [filterRestaurant, setFilterRestaurant] = useState(resList);
+  const [listOfRestaurant, setlistOfRestaurant] = useState([]);
+  const [filterRestaurant, setFilterRestaurant] = useState([]);
   const [searchText, setSearchText] = useState("");
 
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  // async function fetchData() {
-  //   const data = await fetch(
-  //     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.624480699999999&page_type=DESKTOP_WEB_LISTING"
-  //   );
-  //   const jsondata = await data.json();
-  //   console.log(jsondata);
+  async function fetchData() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.624480699999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const jsondata = await data.json();
+    console.log(jsondata);
 
-  //   // setlistOfRestaurants(jsondata.data.cards[2].data.data.cards);
-  //   setlistOfRestaurant(jsondata?.data?.cards[2]?.data?.data?.cards);
-  //   setFilterRestaurant(jsondata?.data?.cards[2]?.data?.data?.cards);
-  // }
+    console.log(
+      jsondata?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants?.info
+    );
+
+    setlistOfRestaurant(
+      jsondata?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setFilterRestaurant(
+      jsondata?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  }
+  console.log(filterRestaurant);
 
   // if (listOfRestaurant.length === 0) {
   //   return <Shimmer />;
@@ -46,7 +57,7 @@ const Body = () => {
             className="filter"
             onClick={() => {
               const filterRestaurant = listOfRestaurant.filter((res) =>
-                res.data.name.toLowerCase().includes(searchText.toLowerCase())
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
               );
               console.log(filterRestaurant);
               setFilterRestaurant(filterRestaurant);
@@ -58,7 +69,7 @@ const Body = () => {
         <button
           onClick={() => {
             let filterdata = listOfRestaurant.filter(
-              (res) => res.data.avgRating > 4
+              (res) => res.info.avgRating > 4.5
             );
             setlistOfRestaurant(filterdata);
           }}
@@ -68,7 +79,7 @@ const Body = () => {
       </div>
       <div className="res-container">
         {filterRestaurant.map((restaurant) => (
-          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,14 +3,10 @@ import { CDN_URL } from "../utills/constants";
 const RestaurantCard = (props) => {
   const { resData } = props;
 
-  const {
-    cloudinaryImageId,
-    name,
-    cuisines,
-    avgRating,
-    costForTwo,
-    deliveryTime,
-  } = resData?.data;
+  console.log("resdata", resData);
+
+  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } =
+    resData?.info;
 
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
@@ -21,7 +17,7 @@ const RestaurantCard = (props) => {
         //   resData.data.cloudinaryImageId
         // }
 
-        src={CDN_URL + cloudinaryImageId}
+        // src={CDN_URL + cloudinaryImageId}
         alt="Biryani"
       />
       {/* <h3>{resData.data.name}</h3>
@@ -32,8 +28,8 @@ const RestaurantCard = (props) => {
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
-      <h4>₹{costForTwo / 100} FOR TWO</h4>
-      <h4>{deliveryTime} minutes</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla?.deliveryTime} minutes</h4>
     </div>
   );
 };
