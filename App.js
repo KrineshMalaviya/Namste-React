211288e4b4b5fe472e3e7b6ff48104fbc7a7d1ea
Krinesh Malaviya <krinesh.malaviya@gmail.com>
2025-12-16 22:46:56 +0530

    Routing using react-router-dom

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
+import About from "./src/components/About";
+import Contact from "./src/components/Contact";
+import Error from "./src/components/Error";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 // const heading = React.createElement(
 //   "div",
@@ -25,11 +29,33 @@ const AppLayout = () => {
   return (
     <div>
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
